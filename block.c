44c768410f442764a40f623da7481178c91fb086
taemkim <temtamk@kakao.com>
2021-07-28 13:03:17 +0900

    [UPDATE]block init function

--- block.c
+++ block.c
@@ -1,1 +1,15 @@
+#include "block.h"
 
+void	block_init(t_block *block)
+{
+	block->nonce = -1;
+	block->time = time(NULL);
+}
+
+int main()
+{
+	t_block block;
+
+	block_init(&block);
+	return (0);
+}
--- block.h
+++ block.h
@@ -3,14 +3,16 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include <ctime>
+#include <time.h>
 #include "sha256.h"
 
 typedef struct s_block
 {
 	char		prevHash[32];
+	char		Hash[32];
 	int32_t		version;
 	time_t		time;
 	uint32_t	nBits;
 	uint32_t	nonce;
+	t_block		next;
 }				t_block;
